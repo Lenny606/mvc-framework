de8d441c458a1cd2b21368eabfdc3b4433866718
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-03-11 21:06:30 +0100

    fix template

--- views/shared/form.mvc.php
+++ views/shared/form.mvc.php
@@ -1,11 +1,11 @@
 <label for="name">Name</label>
-<input type="text" name="name" id="name" value="{{ product['name'] ?? '' }}">
+<input type="text" name="name" id="name" value="{{ product['name'] }}">
 
 {% if (isset($errors["name"])) :  %}
-    <p style="color: red">{{ errors["name"] }}</p>
+    <p style="color: red">{{ $errors["name"] }}</p>
 {% endif  %}
 
 <label for="description">Description</label>
-<textarea  name="description" id="description">{{ product['description'] ?? '' }}</textarea>
+<textarea  name="description" id="description">{{ product['description'] }}</textarea>
 
 <button>Save</button>
